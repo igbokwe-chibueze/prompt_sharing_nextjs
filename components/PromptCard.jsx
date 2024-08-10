d2e039bd7a64ab50ec5694d9699ee27dc14022cb
igbokwe-chibueze <igbokwechibueze@gmail.com>
2024-08-10 12:09:34 +0100

    Made Rating System into its own component.
    Signed-off-by: igbokwe-chibueze <igbokwechibueze@gmail.com>

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 import { BookmarkIcon, CloseFilledIcon, HeartIcon, StarFilledIcon, StarHalfFilledIcon, } from "@constants/icons";
+import Rating from "./Rating";
 
 /**
  * PromptCard Component
@@ -18,9 +19,6 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
 
   const [copied, setCopied] = useState(""); // State to manage copied prompt
 
-  //const [showLoginPopup, setShowLoginPopup] = useState(false); // State to manage popup visibility
-  //const [popupMessage, setPopupMessage] = useState(""); // State to manage popup message
-
   // State to manage if the prompt is liked by the current user
   const [liked, setLiked] = useState(post.likes.includes(session?.user.id));
   // State to manage the number of likes
@@ -31,13 +29,6 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
   // State to manage the number of bookmarks
   const [bookmarkCount, setBookmarkCount] = useState(post.bookmarks.length);
   const [isBookmarking, setIsBookmarking] = useState(false); // State for managing liking action
-
-  // State to manage the rating
-  const [userRating, setUserRating] = useState(null); // Store user's rating
-  const [averageRating, setAverageRating] = useState(Number(post.averageRating) || 0);
-  const [isRating, setIsRating] = useState(false);
-  const [isRateModalOpen, setIsRateModalOpen] = useState(false);
-  const [hoveredRating, setHoveredRating] = useState(null);
 
 
   // Handles profile click - shows login popup if not logged in
@@ -152,69 +143,6 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
     }
   };
 
-
-  // Function to handle rating submission
-
-  useEffect(() => {
-    // Initialize userRating from post data if available
-    setUserRating(post.userRating || null);
-
-    if (session) {
-      const userId = session.user.id
-      // Retrieve the rating given by the current user from the post's ratings array.
-      const existingRating = post.ratings.find(ratingObject => ratingObject.userId.toString() === userId).rating;
-
-      setUserRating(existingRating);
-    }
-
-  }, [post]);
-
-  const handleRate = async (rating) => {
-    if (!session) {
-      router.push(`/login?message=You need to be logged in to rate this post.`);
-      return;
-    }
-
-    // Prevent rating if the user is the creator of the post
-    if (post.creator._id === session.user.id) {
-      return;
-    }
-
-    setUserRating(rating); // Optimistically update user rating
-    setIsRating(true);
-
-    try {
-      const response = await fetch(`/api/prompt/${post._id}/rate`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          userId: session.user.id,
-          rating,
-        }),
-      });
-
-      if (!response.ok) {
-        throw new Error("Failed to update rating");
-      }
-
-      const data = await response.json();
-      setAverageRating(Number(data.averageRating));
-      setUserRating(data.userRating); // Update userRating from server response
-
-    } catch (error) {
-      console.log("Error:", error);
-      // Revert user rating on failure
-      setUserRating(null);
-    } finally {
-      setIsRating(false);
-    }
-  };
-
-  const openRateModal = () => setIsRateModalOpen(true);
-  const closeRateModal = () => setIsRateModalOpen(false);
-
  
   // Check if the post has been updated
   
@@ -305,66 +233,8 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
         <p>{bookmarkCount}</p>
       </div>
 
-      {/* The rating the user gave the post should always be displayed */}
-      <div className="rating_section flex">
-        <button
-          onClick={openRateModal}
-          disabled={post.creator._id === session?.user.id} // Disable if user is the creator
-          className={`mt-4 flex items-center space-x-2 font-inter text-sm text-blue-500 ${post.creator._id === session?.user.id ? "": "cursor-pointer"}`}
-        >
-          <div className="flex space-x-1">
-            {[1, 2, 3, 4, 5].map(star => {
-              if (averageRating >= star) {
-                return (
-                  <StarFilledIcon key={star} className={"w-4 h-4 text-yellow-400"} />
-                );
-              } else if (averageRating >= star - 0.5) {
-                return (
-                  <StarHalfFilledIcon key={star} className={"w-4 h-4 text-yellow-400"} />
-                );
-              }
-              return (
-                <StarFilledIcon key={star} className={"w-4 h-4 text-gray-400"} />
-              );
-            })}
-          </div>
-          <p>({averageRating.toFixed(1)})</p>
-        </button>
-
-        {isRateModalOpen && (
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 flex justify-center items-center z-50 px-10 py-20">
-            <div className="bg-white p-5 rounded shadow-lg relative w-full h-full">
-              <button
-                className="absolute top-2 right-2 text-gray-600 hover:text-gray-900"
-                onClick={closeRateModal}
-              >
-                <CloseFilledIcon className={"w-6 h-6"}/>
-              </button>
-              <h2 className="mt-2 text-center text-xl font-bold mb-4">Rate this prompt</h2>
-              <div className="flex justify-center items-center flex-col">
-                <div className="flex space-x-1">
-                  {[...Array(5)].map((_, index) => (
-                    <button
-                      key={index}
-                      className={`${
-                        (index + 1 <= (hoveredRating || userRating))
-                          ? 'text-yellow-500'
-                          : 'hover:text-yellow-500'
-                      }`}
-                      onClick={() => handleRate(index + 1)}
-                      onMouseEnter={() => setHoveredRating(index + 1)}
-                      onMouseLeave={() => setHoveredRating(null)}
-                    >
-                      <StarFilledIcon />
-                    </button>
-                  ))}
-                </div>
-                <p className="font-inter text-sm text-gray-500 mt-4">Your Current Rating: {userRating || 'None'}</p>
-              </div>
-            </div>
-          </div>
-        )}
-      </div>
+      {/* Rating */}
+      <Rating post={post} session={session}/>
 
       {session?.user.id === post.creator._id && pathName !== "/" && (
         <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
---
+++ components/Rating.jsx
@@ -0,0 +1,133 @@
+import { useState, useEffect } from "react";
+import { StarFilledIcon, StarHalfFilledIcon, CloseFilledIcon } from "@constants/icons";
+import { useRouter } from "next/navigation";
+
+const Rating = ({ post, session }) => {
+
+    const router = useRouter(); // Next.js router for navigation
+
+    const [isRateModalOpen, setIsRateModalOpen] = useState(false);
+    const [hoveredRating, setHoveredRating] = useState(null);
+    const [currentRating, setCurrentRating] = useState(null);
+    const [averageRating, setAverageRating] = useState(Number(post.averageRating) || 0);
+    const [isSubmitting, setIsSubmitting] = useState(false);
+
+    useEffect(() => {
+        if (session) {
+        const userId = session.user.id;
+        const existingRating = post.ratings.find(ratingObject => ratingObject.userId.toString() === userId)?.rating || null;
+        setCurrentRating(existingRating);
+        }
+    }, [post, session]);
+
+    const handleRate = async (rating) => {
+        if (!session) {
+        // Handle redirect to login
+        router.push(`/login?message=You need to be logged in to rate this post.`);
+        return;
+        }
+
+        // Prevent rating if the user is the creator of the post
+        if (post.creator._id === session.user.id) {
+            return;
+        }
+
+        setIsSubmitting(true);
+        setCurrentRating(rating);
+        
+        try {
+        const response = await fetch(`/api/prompt/${post._id}/rate`, {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+            userId: session.user.id,
+            rating: rating,
+            }),
+        });
+
+        if (!response.ok) {
+            throw new Error("Failed to update rating");
+        }
+
+        const data = await response.json();
+            // Update average rating based on data from the server
+            setAverageRating(Number(data.averageRating));
+        } catch (error) {
+            console.log("Error:", error);
+            setCurrentRating(prevRating => prevRating); // Revert rating on failure
+        } finally {
+            setIsSubmitting(false);
+            setIsRateModalOpen(false); // Close the modal after submission
+        }
+    };
+
+  return (
+    <div>
+      {/* Display average rating */}
+      <div className="rating_section flex">
+        <button
+          onClick={() => setIsRateModalOpen(true)}
+          disabled={post.creator._id === session?.user.id} // Disable if user is the creator
+          className={`mt-4 flex items-center space-x-2 font-inter text-sm text-blue-500 ${post.creator._id === session?.user.id ? "": "cursor-pointer"}`}
+        >
+          <div className="flex space-x-1">
+            {[1, 2, 3, 4, 5].map(star => {
+              if (averageRating >= star) {
+                return (
+                  <StarFilledIcon key={star} className={"w-4 h-4 text-yellow-400"} />
+                );
+              } else if (averageRating >= star - 0.5) {
+                return (
+                  <StarHalfFilledIcon key={star} className={"w-4 h-4 text-yellow-400"} />
+                );
+              }
+              return (
+                <StarFilledIcon key={star} className={"w-4 h-4 text-gray-400"} />
+              );
+            })}
+          </div>
+          <p>({averageRating.toFixed(1)})</p>
+        </button>
+
+        {isRateModalOpen && (
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 flex justify-center items-center z-50 px-10 py-20">
+            <div className="bg-white p-5 rounded shadow-lg relative w-full h-full">
+              <button
+                className="absolute top-2 right-2 text-gray-600 hover:text-gray-900"
+                onClick={() => setIsRateModalOpen(false)}
+              >
+                <CloseFilledIcon className={"w-6 h-6"}/>
+              </button>
+              <h2 className="mt-2 text-center text-xl font-bold mb-4">Rate this prompt</h2>
+              <div className="flex justify-center items-center flex-col">
+                <div className="flex space-x-1">
+                  {[...Array(5)].map((_, index) => (
+                    <button
+                      key={index}
+                      className={`${
+                        (index + 1 <= (hoveredRating || currentRating))
+                          ? 'text-yellow-500'
+                          : 'hover:text-yellow-500'
+                      }`}
+                      onClick={() => handleRate(index + 1)}
+                      onMouseEnter={() => setHoveredRating(index + 1)}
+                      onMouseLeave={() => setHoveredRating(null)}
+                      disabled={isSubmitting} // Disable button during submission
+                    >
+                      <StarFilledIcon />
+                    </button>
+                  ))}
+                </div>
+                <p className="font-inter text-sm text-gray-500 mt-4">Your Current Rating: {currentRating || 'None'}</p>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Rating;
