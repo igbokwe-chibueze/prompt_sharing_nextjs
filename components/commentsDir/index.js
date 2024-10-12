41b3158eec38ed9433aefecd6cc7842bbb3de1d9
igbokwe-chibueze <igbokwechibueze@gmail.com>
2024-10-12 10:42:31 +0100

    Archived Comment and CommentList Component because they are no longer needed for now.
    Signed-off-by: igbokwe-chibueze <igbokwechibueze@gmail.com>

--- components/commentsDir/index.js
+++ components/commentsDir/index.js
@@ -1,16 +1,12 @@
-import Comment from "./Comment"
 import CommentBtn from "./CommentBtn"
 import CommentCard from "./CommentCard"
 import CommentCardList from "./CommentCardList"
-import CommentList from "./CommentList"
 import PromptCommentList from "./PromptCommentList"
 
 
 
 export {
     CommentBtn,
-    CommentList,
-    Comment,
     CommentCard,
     CommentCardList,
     PromptCommentList
